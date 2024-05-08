982519f83f3269ed70b183975e15123f7ca26cf1
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-05-08 09:07:10 +0500

    I learn tuple data type in typescript, access the element with index method and access the element using destructing method.

---
+++ index.ts
@@ -0,0 +1,20 @@
+/*********
+ * TUPLE *
+ *********/
+
+// Defining a tuple data type
+// We cannot change the sequence of element in tuple data type this will throw error 
+// let article:readonly [string,number,null] = [18,"John",null]
+let article:readonly [string,number,null] = ["John",18,null]
+
+console.log(article);
+// We can also access there elements using index.
+console.log(article[0]);
+// Push method does not apply on tuples this will throw an error
+// article.push(100);
+
+// We can also access the element by destructing method
+const [name,num,nothing] = article;
+console.log(name);
+console.log(num);
+console.log(nothing);
