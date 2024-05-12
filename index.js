4f74f5a4752e7033b8241fcea8be4ba3bc249762
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-05-12 20:49:54 +0500

    I solve extra 3 question for my practice

--- index.js
+++ index.js
@@ -15,4 +15,41 @@ const [name, num, nothing] = article;
 console.log(name);
 console.log(num);
 console.log(nothing);
+let Product1 = ["Shirts", 450, 10];
+let Product2 = ["Socks", 100, 29];
+// Display first product 
+const [product, price, Quantity] = Product1;
+console.log(`Product name = ${product},Price = ${price},Quantity = ${Quantity}`);
+// Display second product 
+const displayProduct = (Product) => {
+    const [product, price, Quantity] = Product;
+    console.log(`Product name = ${product},Price = ${price},Quantity = ${Quantity}`);
+};
+displayProduct(Product2);
+let Subject = [["Math", 30], ["English", 30], ["Science", 40]];
+let add = (Subject[0][1] + Subject[1][1] + Subject[2][1]) / 3;
+if (add >= 80) {
+    console.log(`Grade is A+`);
+}
+else if (add >= 70 && add <= 79) {
+    console.log(`Grade is A`);
+}
+else if (add >= 60 && add <= 69) {
+    console.log(`Grade is B`);
+}
+else if (add >= 50 && add <= 59) {
+    console.log(`Grade is C`);
+}
+else if (add >= 40 && add <= 49) {
+    console.log(`Grade is D`);
+}
+else {
+    console.log(`Grade is F`);
+}
+let weatherInformation = ["Jhang", 43, "Hot"];
+const displayWeather = (weather) => {
+    let [city, Temperature, Weather] = weather;
+    console.log(`City name is ${city}, Temperature in Celsius is ${Temperature}C, ;Weather Condition is ${Weather}`);
+};
+displayWeather(weatherInformation);
 export {};
--- index.ts
+++ index.ts
@@ -18,3 +18,71 @@ const [name,num,nothing] = article;
 console.log(name);
 console.log(num);
 console.log(nothing);
+
+//! Question 1:
+// You are building a simple e-commerce application and need to store product information. Define a tuple type called ProductInfo to represent each product, containing the following elements:
+
+// Product name (string)
+// Price (number)
+// Quantity in stock (number)
+// Create two product instances using this tuple type and display their information.
+
+type ProductInfo = readonly [string,number,number]
+let Product1:ProductInfo = ["Shirts",450,10];
+let Product2:ProductInfo = ["Socks",100,29];
+
+// Display first product 
+const [product,price,Quantity] = Product1;
+console.log(`Product name = ${product},Price = ${price},Quantity = ${Quantity}`);
+
+// Display second product 
+const displayProduct = (Product:ProductInfo) =>{
+    const [product,price,Quantity] = Product;
+    console.log(`Product name = ${product},Price = ${price},Quantity = ${Quantity}`);
+}
+displayProduct(Product2);
+
+//! Question 2:
+// You are creating a student management system and want to keep track of student grades for different subjects. Define a tuple type called SubjectGrade to represent a subject and its corresponding grade, containing the following elements:
+
+// Subject name (string)
+// Grade (number)
+// Create an array of SubjectGrade tuples to store the grades for a student in three different subjects: Math, English, and Science. Calculate and display the average grade for the student.
+type SubjectGrade = readonly [string,number]
+
+let Subject:SubjectGrade[] = [["Math",30],["English",30],["Science",40]];
+let add = (Subject[0][1] + Subject[1][1] + Subject[2][1])/3;
+if(add>=80){
+    console.log(`Grade is A+`)
+}else if(add>=70 && add<=79){
+    console.log(`Grade is A`)
+}
+else if(add>=60 && add<=69){
+    console.log(`Grade is B`)
+}
+else if(add>=50 && add<=59){
+    console.log(`Grade is C`)
+}
+else if(add>=40 && add<=49){
+    console.log(`Grade is D`)
+}
+else {
+    console.log(`Grade is F`);
+    
+}
+//! Question 3:
+// You are working on a weather application, and you need to store weather data for different cities. Define a tuple type called WeatherData to represent the weather information, containing the following elements:
+
+// City name (string)
+// Temperature in Celsius (number)
+// Weather condition (string)
+// Create a function called displayWeather that takes an array of WeatherData tuples as input and displays the weather information for each city in a user-friendly format.
+
+type WeatherData = readonly [string,number,string]
+let weatherInformation:WeatherData = ["Jhang",43,"Hot"];
+
+const displayWeather = (weather:WeatherData) =>{
+    let [city,Temperature,Weather] = weather;
+    console.log(`City name is ${city}, Temperature in Celsius is ${Temperature}C, ;Weather Condition is ${Weather}`);
+}
+displayWeather(weatherInformation);
